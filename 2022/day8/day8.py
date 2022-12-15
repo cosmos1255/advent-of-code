91c6026fc688f9bb679aef3d35686846d40d8e50
cosmos1255 <davidseyrich@outlook.com>
2022-12-15 07:12:22 -0500

    pushing up changes

--- 2022/day8/day8.py
+++ 2022/day8/day8.py
@@ -4,6 +4,7 @@ Link: https://adventofcode.com/2022/day/8"""
 
 import os
 import sys
+import re
 
 scripts_path = os.getcwd()
 scripts_path_split = scripts_path.split('/')
@@ -18,13 +19,35 @@ def parseInput(filename):
         input = f.read()
     
     # parse through input here
-    parsedInput = ''
+    parsedInput = []
+    for line in input.split('\n'):
+        parsedInput.append(re.findall(r'\d', line))
+    
+    for i in range(len(parsedInput)):
+        for j in range(len(parsedInput[0])):
+            parsedInput[i][j] = int(parsedInput[i][j])
     
     return parsedInput
 
 def partA(input):
     print("Part A")
-    return 0
+    
+    num_visible = 4 * len(input)-1
+    counter = 0
+    
+    for i in range(1, len(input[1:-1])+1):
+        for j in range(1, len(input[0][1:-1])+1):
+            up = [input[i][j-k] for k in range(1, j+1)]
+            down = [input[i][j+k] for k in range(1, len(input) - j)]
+            left = [input[i-k][j] for k in range(1, i+1)]
+            right = [input[i+k][j] for k in range(1, len(input) - i)]
+        
+            for d in (up, down, left, right):
+                if (max(d) < input[i][j]):
+                    num_visible += 1
+                    break
+    
+    return num_visible
 
 def partB(input):
     print("Part B")
@@ -35,8 +58,8 @@ def entry():
     input = parseInput("day8_input.txt")
     
     # uncomment below to submit part A
-    # ansA = partA(input)
-    # print(ansA)
+    ansA = partA(input)
+    print(ansA)
     # submit(1, ansA, 2022, 8)
     
     # uncomment below to submit part B
